e25963eb05a5725c5438cc1d5074d4562b6898c2
Pushpendra Sharma <pushpendrash718@gmail.com>
2024-07-07 16:34:21 +0530

    heroku

--- app.py
+++ app.py
@@ -11,7 +11,13 @@ from models import db, User, Item
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///instance/flaskr.sqlite')
+
+# Update the database URL to handle Heroku's PostgreSQL URL
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///instance/flaskr.sqlite')
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
