c4878d5516f331f25678d472409c879d7df1bb27
Pushpendra Sharma <pushpendrash718@gmail.com>
2024-09-28 10:16:42 +0530

    Updated URL Database

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///local.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('HEROKU_POSTGRESQL_BRONZE_URL') or 'sqlite:///local.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
